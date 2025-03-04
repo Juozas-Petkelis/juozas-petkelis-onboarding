308240136c8d7cf3bbd9e89910b370d50b19acb5
Juozas Petkelis <juozas.petkelis@callstack.com>
2025-03-04 15:33:35 +0200

    chore: lint , ts and prettier config
    remove initial app.tsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,28 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  plugins: ['module-resolver', 'prettier', 'import'],
+  rules: {
+    'module-resolver/use-alias': 2,
+    'prettier/prettier': ['error'],
+    'import/no-unresolved': 'error',
+    'import/named': 'error',
+    'import/default': 'error',
+    'import/namespace': 'off',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external', 'internal']],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/no-extraneous-dependencies': 'warn',
+    'import/prefer-default-export': 'off',
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@types': './src/types',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+  ],
 };
